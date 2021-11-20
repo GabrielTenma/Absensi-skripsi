d38c58b64a486bbf8797a072b3767620d4f930b3
Gab! <44790547+GabrielTenma@users.noreply.github.com>
2021-11-20 11:24:03 +0700

    testing api

--- Absensi/testing.py
+++ Absensi/testing.py
@@ -3,5 +3,5 @@ import App.config.variable.ApplicationConstant as appConfig
 import App.util.CommonUtil as util
 
 # TESTING CALL API x
-IMAGE_SAMPLE = util.checkPath(appConfig.TAKE_PICTURE_FILENAME)
+IMAGE_SAMPLE = util.checkPath("App/assets/kocak.jpeg")
 print(api.matchImagePerson('20', IMAGE_SAMPLE))
